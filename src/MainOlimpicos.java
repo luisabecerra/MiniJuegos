e6f6fd38fc175dbf5cff3ffa205003c61a22393e
LUISA BECERRA - CAMILO MORA <>
2016-03-15 17:02:51 -0500

    Mini juegos olimpicos

--- src/MainOlimpicos.java
+++ src/MainOlimpicos.java
@@ -1,9 +1,19 @@
-package mainolimpicos;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-
+/**
+ *
+ * @author 
+ */
 import java.util.Scanner;
 import java.util.ArrayList;
 public class MainOlimpicos {
+
+
+
 
     public static Scanner leer=new Scanner (System.in);
     public static void main(String[] args) {
